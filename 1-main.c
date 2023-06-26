713085df7ef44c701b2a19edbbbbc986aeefeb28
houria <h1344308@gmail.com>
2023-06-26 05:02:54 -0700

    task1

---
+++ 1-main.c
@@ -0,0 +1,13 @@
+
+
+int _printf(const char *format, ...);
+
+int main(void)
+{
+    int num = 10;
+
+    _printf("Number: %d\n", num);
+
+    return 0;
+}
+
--- main.h
+++ main.h
@@ -4,4 +4,7 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _print_str(char *str);
+int write_char(char c);
+int write_string(char *str);
+int print_number(int num, char *num_buffer, int char_count);
 #endif
